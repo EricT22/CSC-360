605541ac6077351f8997953201c089892db8b999
eric <phantomenace87@gmail.com>
2026-01-31 14:17:05 -0800

    hw done

--- src/BitManipulation/BitOperations.java
+++ src/BitManipulation/BitOperations.java
@@ -2,24 +2,54 @@ package BitManipulation;
 
 public class BitOperations {
 
-    public static int left(byte b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'left'");
+    public static int left(byte _byte) {
+        int b = (int)_byte;
+        int mask = 0x000000FF;
+
+        b &= mask;
+
+        b >>= 4;
+
+
+        return (byte)b;
     }
 
-    public static int right(byte b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'right'");
+    public static int right(byte _byte) {
+        int b = (int)_byte;
+
+        int mask = 0x0000000F;
+
+        b &= mask;
+
+        return (byte)b;
     }
 
-    public static int sixbits(byte b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sixbits'");
+    public static int sixbits(byte _byte) {
+        int b = (int)_byte;
+        int mask = 0x000000FC;
+
+        b &= mask;
+
+        b >>= 2;
+
+
+        return (byte)b;
     }
 
-    public static int lrswap(short s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lrswap'");
+    public static int lrswap(short _in) {
+        int mask = 0x0000FFFF;
+
+        int l = (int)_in;
+
+        l &= mask;
+        l >>= 8;
+        
+        int r = (int)_in;
+        
+        r <<= 8;
+        r &= mask;
+
+        return (short)(l | r);
     }
     
 }
