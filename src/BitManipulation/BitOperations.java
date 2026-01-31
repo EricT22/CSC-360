b27f7cb8df36ccfba4271c16c1fd8042e702208c
eric <phantomenace87@gmail.com>
2026-01-31 12:08:17 -0800

    Setting up hw

---
+++ src/BitManipulation/BitOperations.java
@@ -0,0 +1,25 @@
+package BitManipulation;
+
+public class BitOperations {
+
+    public static int left(byte b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'left'");
+    }
+
+    public static int right(byte b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'right'");
+    }
+
+    public static int sixbits(byte b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sixbits'");
+    }
+
+    public static int lrswap(short s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lrswap'");
+    }
+    
+}
