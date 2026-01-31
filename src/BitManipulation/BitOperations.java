266ccb59f411e278843d3adb4929f88ab9fa8f30
eric <phantomenace87@gmail.com>
2026-01-31 15:40:16 -0800

    finished hw

--- src/BitManipulation/BitOperations.java
+++ src/BitManipulation/BitOperations.java
@@ -52,14 +52,37 @@ public class BitOperations {
         return (short)(l | r);
     }
 
-    public static long expander(byte b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'expander'");
+    public static int expander(byte b) {
+        int e = (int)b;
+        e &= 0x0000FFFF;
+
+        int b7 = (e & 0b00100000) << 2;
+        int b6 = (e & 0b00010000) << 2;
+        int b5 = (e & 0b00000100) << 3;
+        int b4 = (e & 0b00001000) << 1;
+        int b3 = (e & 0b00000100) << 1;
+        int b2 = (e & 0b00001000) >> 1;
+        int b1 = (e & 0b00000010);
+        int b0 = (e & 0b00000001);
+        
+
+        return b7 | b6 | b5 | b4 | b3 | b2 | b1 | b0;
     }
 
-    public static long keyextractor(short key, int pos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyextractor'");
+    public static int keyextractor(short key, int pos) {
+        int k = (int)key;
+        int mask = 0x0000FFFF;
+
+        k &= mask;
+
+        for (int i = 0; i < pos; i ++){
+            // wrap
+            k = ((k << 1) & mask) | (k >> 15);
+        }
+
+        k = lrswap((short)k);
+
+        return (byte)k;
     }
     
 }
